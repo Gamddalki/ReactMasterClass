eeb3e2c8de2fb58cb9f10c62c16bf176a0267528
Gamddalki <hannahk01@naver.com>
2023-02-14 18:32:49 +0900

    Add ThemeProvider

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  main: "white",
+};
+
+export const darkTheme: DefaultTheme = {
+  main: "black",
+};
