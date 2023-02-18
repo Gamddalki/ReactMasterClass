537975b22a49482926c8c404aff0d1e3e219f09d
Gamddalki <hannahk01@naver.com>
2023-02-19 01:11:20 +0900

    Add conditions to toDo

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,9 +2,27 @@ import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, condition, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, condition: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
+      {condition !== "TO_DO" && <button onClick={onClick}>🛫</button>}
+      {condition !== "DOING" && <button onClick={onClick}>✈️</button>}
+      {condition !== "DONE" && <button onClick={onClick}>🛬</button>}
     </li>
   );
 }
