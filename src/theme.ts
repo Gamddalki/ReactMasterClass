90dc278d2de0d5e8185ff80069083c4d29e8362a
Gamddalki <hannahk01@naver.com>
2023-02-19 12:41:18 +0900

    Update CSS

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,17 @@
 import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "#f5f6fa",
+  bgColor: "#ff6b81",
   textColor: "#2f3640",
   accentColor: "#e84118",
-  cardColor: "white",
+  boardColor: "#f1f2f6",
+  cardColor: "#ffffff",
 };
 
 export const darkTheme: DefaultTheme = {
   bgColor: "#2f3640",
   textColor: "#f5f6fa",
   accentColor: "#e84118",
+  boardColor: "#f1f2f6",
   cardColor: "#2d3436",
 };
